65f3327e3323a483f2f3f9e0b03e95d36e61f398
Ralph Avalon <amoedo.raphael@gmail.com>
2016-12-03 11:32:50 -0200

    Adding test config and first test

---
+++ src/main/java/com/graphs/Main.java
@@ -0,0 +1,9 @@
+package com.graphs;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.print(9);
+	}
+
+}
---
+++ src/test/java/com/graphs/MainTest.java
@@ -0,0 +1,36 @@
+package com.graphs;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	    System.setErr(null);
+	}
+
+	@Test
+	public void test() {
+		Main.main(null);
+		final String printedContent = outContent.toString();
+		assertTrue(printedContent.equals("9"));
+	}
+
+}
